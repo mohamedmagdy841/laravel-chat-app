af454532dd94d9a3d04213a3b53e93d07c4de717
Mohamed Magdy Gamal <64127744+mohamedmagdy841@users.noreply.github.com>
2025-04-22 06:15:18 +0200

    created routes for channels and web

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
+use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => redirect()->route('login'));
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::middleware('auth')->group(function (){
+    Route::controller(ChatController::class)->group(function () {
+        Route::get('/users', 'index')->name('users');
+        Route::get('/chat/{receiverId}', 'chat')->name('chat');
+        Route::post('/chat/{receiverId}/send', 'sendMessage');
+        Route::post('/chat/typing', 'typing');
+        Route::post('/online', 'setOnline');
+        Route::post('/offline', 'setOffline');
+    });
+});
